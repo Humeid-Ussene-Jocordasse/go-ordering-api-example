815e4f3e0a21218fc25181a7225ec338ab0c5e5d
Humeid Jocordasse <humeid.jocordasse@vm.co.mz>
2024-07-26 21:52:29 +0200

    wip: add handlers

---
+++ application/routes.go
@@ -0,0 +1,21 @@
+package application
+
+import (
+	"net/http"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+)
+
+func loadRoutes() *chi.Mux {
+	// create the main instance of chi router
+	router := chi.NewRouter()
+
+	// use logger as a middleware
+	router.Use(middleware.Logger)
+
+	router.Get("/", func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+	})
+	return router
+}
