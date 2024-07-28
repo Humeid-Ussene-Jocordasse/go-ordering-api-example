d44cbd37012658c9c209634ed394b53d7d4fff96
Humeid Jocordasse <humeid.jocordasse@vm.co.mz>
2024-07-28 15:49:45 +0200

    wip adding redis configuration, using a way of managing channel

--- application/app.go
+++ application/app.go
@@ -3,16 +3,19 @@ package application
 import (
 	"context"
 	"fmt"
+	"github.com/redis/go-redis/v9"
 	"net/http"
 )
 
 type App struct {
 	router http.Handler
+	rdb    *redis.Client
 }
 
 func New() *App {
 	app := &App{
 		router: loadRoutes(),
+		rdb:    redis.NewClient(&redis.Options{}),
 	}
 
 	return app
@@ -24,9 +27,29 @@ func (a *App) Start(ctx context.Context) error {
 		Handler: a.router,
 	}
 
-	err := server.ListenAndServe()
+	fmt.Println("Connecting to redis-db")
+	err := a.rdb.Ping(ctx).Err()
+	if err != nil {
+		return fmt.Errorf("could connect to redis instance %w", err)
+	}
+
+	fmt.Println("Starting application")
+
+	ch := make(chan error, 1)
+	//go func() {
+	//	err = server.ListenAndServe()
+	//	if err != nil {
+	//		return fmt.Errorf("failed to start server: %w", err)
+	//	}
+	//}()
+	err = <-ch
+	go startServer(server.ListenAndServe(), ch)
 	if err != nil {
 		return fmt.Errorf("failed to start server: %w", err)
 	}
 	return err
+}
+
+func startServer(err error, ch chan error) {
+	ch <- fmt.Errorf("failed to start server: %w", err)
 }
